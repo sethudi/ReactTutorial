ee2864223d3ae5b1ab9876b5086d31b145c5cad2
sethudi <stmapunya@gmail.com>
2021-08-12 17:10:52 +0200

    'Created'

--- client/src/BookLibrary.js
+++ client/src/BookLibrary.js
@@ -1,16 +1,51 @@
 import React from 'react';
 import axios from 'axios';
+import Editicon from '@material-ui/icons/Edit';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import './BookLibrary.css';
 
 class BookLibrary extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: [],
+        };
+    }
+
     componentDidMount() {
         axios(process.env.REACT_APP_SERVER_URL)
-            .then(result =>console.log(result))
-            .catch(error =>console.log(error));
+            .then(result => this.setState({ books: result.data }))
+            .catch(error => console.log(error));
     }
 
     render() {
-        return (<div>Library</div>);
+        let books = this.state.books.map((book) => {
+            let date =book.published.toString().substr(0,4);
+            return (
+                <tr key={book.id}>
+                    <td>{book.author}</td>
+                    <td>{book.title}</td>
+                    <td>{date}</td>
+                    <td><Editicon /></td>
+                    <td><DeleteForeverIcon /></td>
+                </tr>
+            )
+        });
+        console.log('render', this.state.books);
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr><th>Author</th><th>Title</th><th>Published</th></tr>
+                    </thead>
+                    <tbody>
+                        {books}
+                    </tbody>
+
+                </table>
+            </div>
+        );
     }
 }
 
---
+++ client/src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+
+  return (
+    <div className="header">
+        <ul className="header-left">
+            <li className="logo"><a href="/">Book Library</a></li>
+        </ul>
+
+        <ul className="header-right">
+            <li><a href="/">Created</a></li>
+            <li><a href="/">View</a></li>
+        </ul>
+    </div>
+  );
+}
+
+export default Header;
