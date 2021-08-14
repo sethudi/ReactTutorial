0135eda1f6d5dd011121c28c1f25da7632fead2d
sethudi <stmapunya@gmail.com>
2021-08-14 17:29:35 +0200

    'Created'

--- client/src/Book.js
+++ client/src/Book.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Book.css';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
+import FlashMessage  from './FlashMessage';
 
 class Book extends React.Component {
 
@@ -27,6 +28,7 @@ class Book extends React.Component {
       author: '',
       title: '',
       published: '',
+      submitAttempts: 0,
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -50,7 +52,7 @@ class Book extends React.Component {
       const value = this.state[field];
 
       if (!value.match(rule)) {
-        this.showMessage(message);
+        this.setState({ message: message, submitAttempts: this.state.submitAttempts+1});
         return false;
       }
     }
@@ -58,13 +60,6 @@ class Book extends React.Component {
 
   }
 
-  showMessage(message) {
-    this.setState({ message: message });
-
-    setTimeout(() => {
-      this.setState({ message: '' });
-    }, 3000);
-  }
 
   handleSubmit(event) {
     event.preventDefault();
@@ -108,7 +103,7 @@ class Book extends React.Component {
           <label htmlFor="published">Published:</label>
           <input value={this.state.published} onChange={this.handleChange} type="text" name="published" id="published" />
           <input type="submit" value="save" />
-          <div className="message">{this.state.message}</div>
+          <FlashMessage key ={this.state.submitAttempts} message={this.state.message} duration='3000' />
         </form>
 
       </div>
